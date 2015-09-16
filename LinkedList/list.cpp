9f668b1e40526da8365e207adbfcd1e9846daf01
kiangmusa <kiangmusa23@gmail.com>
2015-09-16 22:04:43 +0700

    produk1(insertfirst/last).cpp
    Angky fajriati / 1301141221

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include "list.h"
+
+address alokasi (infotype x)
+{
+    address p;
+    p = new elmlist;
+    info(p)=x;
+    return p;
+}
+
+void baru (infotype *x)
+{
+    infotype y;
+    cout<<"Input data"<<endl;
+    cout<<"Nama     : ";
+    cin>>y.nama;
+    cout<<"ID       : ";
+    cin>>y.id;
+    cout<<"tglmasuk : ";
+    cin>>y.tgl;
+    cout<<"Jenis    : ";
+    cin>>y.jenis;
+    cout<<"Harga    : ";
+    cin>>y.harga;
+    *x=y;
+}
 void insertfirst (list *L, address P)
 {
     if((*L).first==NULL)
